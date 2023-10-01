da66742853eb9a00f5813780210a29bf59732ccc
Collins Wanjiru <waruiwanjiru47@gmail.com>
2023-10-01 18:36:41 +0300

    Heading for the application

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 function App() {
   return (
-    <div>
-      
+    <div className='to_app'>
+      <h1>Welcome To My task Application</h1>
+      <p>This application is to help you to manage your tasks for the day</p>
     </div>
   )
 }
 
-export default App
+export default App;
