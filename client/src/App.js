ec9ce675411248820f68404bddb6b0b5e1eb978b
Collins Wanjiru <waruiwanjiru47@gmail.com>
2023-10-02 11:24:53 +0300

    added some logic to allow user to create tasks in the application

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,41 @@
-import React from 'react';
-import './index.css';
+import {useState, useEffect} from 'react';
+
+const API_BASE = "https://localhost:3000";
 
 
 function App() {
+  const [todo, setTodos] = useState([]);
+  const [popupActive, setpopupActive] = useState(false);
+  const [newTodo, setNewTodo] = useState("");
+
+
+  useEffect(() => {
+    GetTodos();
+  }, [])
+
+  const GetTodos = () => {
+    fetch(API_BASE + "/todos")
+        .then(res => res.json())
+        .then(data => setTodos(data))
+        .catch(err => console.error("Error:", err));
+  }
+
   return (
     <div className='app'>
       <h1>Welcome To My task Application</h1>
       <h4>These are your tasks</h4>
 
       <div className='todos'>
-        <div className='todo'>
-          <div className='checkbox'></div>
-          <div className='text'>Get the bread</div>
-          <div className='delete_todo'>x</div>
-        </div>
+        {todo.map(todo => (
+                <div className='todo'>
+                  <div className='checkbox'></div>
 
-        <div className='todo is-complete'>
-          <div className='checkbox'></div>
-          <div className='text'>Get the milk</div>
-          <div className='delete_todo'>x</div>
-        </div>
+                  <div className='text'>{todo.text}</div>
+
+                  <div className='delete_todo'>X</div>
+                </div>
+        ))}
+
       </div>
     </div>
 
