bc6d1f9ae1948837f3494f6647bf1d00e1e9bee4
Collins Wanjiru <waruiwanjiru47@gmail.com>
2023-10-04 13:28:12 +0300

    migrated the database from local storage to online

--- api/server.js
+++ api/server.js
@@ -2,13 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
+require('dotenv').config();
+
 // creating the server
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb://127.0.0.1:27017/mern_todo", {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => console.log("Connected to DB"))
@@ -63,5 +65,5 @@ app.put('/todo/update/:id', async(req, res) => {
 })
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Server started at port ${port}`));
