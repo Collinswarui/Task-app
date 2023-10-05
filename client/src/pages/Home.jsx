b0b4931e43fe09da0daf0784a4a7fe9b665ea1e3
Collins Wanjiru <waruiwanjiru47@gmail.com>
2023-10-05 11:54:29 +0300

    modified the API_BASE to use the backend that is hosted on a live environment

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {useState, useEffect} from 'react';
 
-const API_BASE = "http://localhost:3001";
+const API_BASE = "https://task-backend-43vh.onrender.com";
 
 export const Home = () => {
   
